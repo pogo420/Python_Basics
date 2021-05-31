8b737012aa214a3832aaf61929a9fb35bd247c45
pogo420 <am0072008@gmail.com>
2021-05-31 13:40:15 +0530

    singleton updates

--- DesignPatterns/singleton.py
+++ DesignPatterns/singleton.py
@@ -1,5 +1,5 @@
-# single object in a lifecycle
-
+# Single object in a lifecycle
+# Careful with testing: Make sure we have 1 singleton for test and 1 for prod.
 
 def decorator(class_):  # class object
     instance = {}  # dictionary for {class -> object}
@@ -19,8 +19,45 @@ class Database:
         print("Connecting to DB")
 
 
+# Mono State, Variation of singleton where state of
+class MonoState:
+    __shared_state = {}
+
+    def __new__(cls, *args, **kwargs):
+        # static method is called when object is created
+        obj = super().__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls.__shared_state
+        return obj
+
+
+class CFO(MonoState):
+
+    def __init__(self):
+        self.name = ""
+        self.money=0
+
+    def __str__(self):
+        return f"""CFO { {
+            "name": self.name,
+            "money": self.money} } """
+
+
+class ObjectTest:
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, *args, **kwargs)
+
+
 if __name__ == "__main__":
     d1 = Database(1)
     d2 = Database(2)
-
     print(d1 == d2)
+
+    # Mono state example
+    cfo1 = CFO()
+    cfo2 = CFO()
+    print(cfo1, cfo2)
+    cfo1.money = 100000
+    print(cfo1, cfo2)
+
+    print(ObjectTest())
