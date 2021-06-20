50332ed6937675bd208fae88fdcb0438f7b863a5
Arnab Mukherjee <am0072008@gmail.com>
2021-06-20 22:00:14 +0530

    sync server

---
+++ AsyncIoDetails/sync_server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from time import sleep
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    user: str = request.args.get('user')
+    sleep(5)  # sleep for 5 seconds, to simulate delay in request
+    return user.upper()
+
+#  curl -X GET "http://127.0.0.1:5000/?user=arnabsudesh"
+
+
+if __name__ == '__main__':
+    app.run()
