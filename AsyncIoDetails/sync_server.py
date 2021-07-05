abe50bf0ee2517efb4455483b9f52f30be6f8308
Arnab Mukherjee <am0072008@gmail.com>
2021-07-06 00:36:45 +0530

    thread issues

--- AsyncIoDetails/sync_server.py
+++ AsyncIoDetails/sync_server.py
@@ -1,16 +1,18 @@
 from flask import Flask, request
 from time import sleep
+from random import randint
+import sys
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     user: str = request.args.get('user')
-    sleep(5)  # sleep for 5 seconds, to simulate delay in request
+    sleep(randint(1,5))  # sleep for 5 seconds, to simulate delay in request
     return user.upper()
 
 #  curl -X GET "http://127.0.0.1:5000/?user=arnabsudesh"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=int(sys.argv[1]))
